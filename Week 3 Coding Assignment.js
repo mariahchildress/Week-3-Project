a8c373ad5173b98db63426034430eaefcec7472a
Mariah Childress <98929724+mariahchildress@users.noreply.github.com>
2022-02-18 19:17:25 -0600

    Friday Night Update

--- Week 3 Coding Assignment.js
+++ Week 3 Coding Assignment.js
@@ -1,7 +1,20 @@
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]
-for (var i = 0; i < 10; i++){
+for (var i = 0; i < ages.length; i++){
     ages[7] - ages[0];
 }
 console.log(i);
 
 ages.push('38');
+for (var i = 0; i < ages.length; i++){
+    ages[8] - ages[0];
+}
+console.log(i);
+
+function ArrayAvg(ages){
+    var i = 0, summ = 0, length = ages.length;
+    while (i < ages.length){
+        summ = summ + ages[i++];
+    }
+    return summ / ages.length;
+}
+console.log(i);
