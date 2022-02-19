6b1a7c5bc3919aa0c4423d73c0aab85440985db5
Mariah Childress <98929724+mariahchildress@users.noreply.github.com>
2022-02-18 21:23:59 -0600

    added html file

--- Week 3 Coding Assignment.js
+++ Week 3 Coding Assignment.js
@@ -1,15 +1,18 @@
+// Question 1a //
+
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]
 for (var i = 0; i < ages.length; i++){
     ages[7] - ages[0];
 }
 console.log(i);
-
+// question 1b //
 ages.push('38');
 for (var i = 0; i < ages.length; i++){
     ages[8] - ages[0];
 }
 console.log(i);
 
+// question 1c //
 function ArrayAvg(ages){
     var i = 0, summ = 0, length = ages.length;
     while (i < ages.length){
@@ -18,3 +21,21 @@ function ArrayAvg(ages){
     return summ / ages.length;
 }
 console.log(i);
+
+// question 2 //
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+
+// question 2a //
+avg = names.join(' ').length / names.length;
+console.log(avg);
+
+// question 2b //
+// put loop here //
+
+console.log(elements.join(' '));
+
+// question 5 //
+let nameLengths = [3, 5, 3, 5, 4, 3];
+
+// question 6 //
+// write a loop to calculate the sum of the elements in the nameLengths array //
